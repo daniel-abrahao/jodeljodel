c63b333257ef1a3cb1de5b6f04640fe08d032efa
Daniel Abrahao <daniel@preface.com.br>
2014-06-18 23:00:15 -0300

    Fixing issues with Typographer due CakePHP update
    Fixing main loop for parse helpers names on TypeView
    Also, creating proper documentation on main typographer files
    And finally, making code beautifier

--- src/cake/app/plugins/typographer/controllers/components/type_layout_scheme_picker.php
+++ src/cake/app/plugins/typographer/controllers/components/type_layout_scheme_picker.php
@@ -9,19 +9,28 @@
  *
  * @copyright     Copyright 2010-2013, Preface Design LTDA (http://www.preface.com.br)
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- * @link          https://github.com/prefacedesign/jodeljodel Jodel Jodel public repository 
+ * @link          https://github.com/prefacedesign/jodeljodel Jodel Jodel public repository
  */
 
-// @todo Maybe, there is no need of a component, maybe just adding an extra layer between
-//    Controller and AppController can solve this issue.
+/**
+ * Class TypeLayoutSchemePickerComponent
+ */
+class TypeLayoutSchemePickerComponent extends Object {
+	/**
+	 * Holds a reference to the current controller
+	 * @var AppController
+	 */
+	protected $Controller;
 
-class TypeLayoutSchemePickerComponent extends Object 
-{
-	function initialize(&$controller, $settings = array()) 
-	{
-		$this->controller =& $controller; //importante senão depois não temos acesso ao Controller
+	/**
+	 * Receives the controller reference and stores it on a class property
+	 *
+	 * @param AppController $Controller
+	 */
+	function initialize (&$Controller) {
+		$this->Controller =& $Controller;
 	}
-	
+
 	/* Por esta função se escolhe o modelo de layout que o controller quer
 	   usar para renderizar uma certa view, e também a partir da variável
 	   do controller $helpers_estilista, nós sabemos quais os helpers se
@@ -34,42 +43,39 @@ class TypeLayoutSchemePickerComponent extends Object
 											 //passadas para a view como uma variável ambiental, se não estiver, as classes automáticas
 											 //serão registradas (pela própria Fabriquinha no callback) como já utilizadas.
 	*/
-		
-	function pick($layout_scheme)
-	{
-		App::import('Config', 'Typographer.' . $layout_scheme . '_config');		
+
+	/**
+	 * This function
+	 * @param string $layout_scheme
+	 */
+	function pick ($layout_scheme) {
+		App::import('Config', 'Typographer.' . $layout_scheme . '_config');
 		$c_layout_scheme = Inflector::camelize($layout_scheme);
-		
+
 		//carrega os instrumentos e as configurações deste layout específico/
-		$tools = Configure::read('Typographer.' . $c_layout_scheme . '.tools');
+		$tools                  = Configure::read('Typographer.' . $c_layout_scheme . '.tools');
 		$used_automatic_classes = Configure::read('Typographer.' . $c_layout_scheme . '.used_automatic_classes');
-	
-		foreach($this->controller->helpers as $helper => $params)
-		{
-			if (is_array($params))
-			{
-				if (isset($params['receive_tools']))
-				{
-					$this->controller->helpers[$helper]['tools'] = $tools;
+
+		foreach ($this->Controller->helpers as &$params) {
+			if (is_array($params)) {
+				if (isset($params['receive_tools'])) {
+					$params['tools'] = &$tools;
 					unset($params['receive_tools']);
 				}
-				
-				if (isset($params['receive_automatic_classes']))
-				{
-					$this->controller->helpers[$helper]['used_automatic_classes'] = $used_automatic_classes;
+
+				if (isset($params['receive_automatic_classes'])) {
+					$params['used_automatic_classes'] = &$used_automatic_classes;
 					unset($params['used_automatic_classes']);
 				}
 			}
 		}
-		
-		if (!isset($this->controller->view) || $this->controller->view == 'View')
-		{
-			$this->controller->view = 'Typographer.Type';
+
+		if (!isset($this->Controller->view) || $this->Controller->view == 'View') {
+			$this->Controller->view = 'Typographer.Type';
 		}
-		
-		$this->controller->set('used_automatic_classes', $used_automatic_classes);
-		$this->controller->set($tools);
-		$this->controller->set('layout_scheme', $layout_scheme);
+
+		$this->Controller->set('used_automatic_classes', $used_automatic_classes);
+		$this->Controller->set($tools);
+		$this->Controller->set('layout_scheme', $layout_scheme);
 	}
 }
-?>
--- src/cake/app/plugins/typographer/views/type.php
+++ src/cake/app/plugins/typographer/views/type.php
@@ -9,103 +9,111 @@
  *
  * @copyright     Copyright 2010-2013, Preface Design LTDA (http://www.preface.com.br)
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- * @link          https://github.com/prefacedesign/jodeljodel Jodel Jodel public repository 
+ * @link          https://github.com/prefacedesign/jodeljodel Jodel Jodel public repository
  */
 
-//cria os aliases para os helpers -- e trata os helpers com o coringa '*'
-//@todo documentate this in English
-class TypeView extends View
-{
 /**
- * 
- * 
- * @access public
+ * Class TypeView
+ *
+ * Deals with the asterisk in the helpers name and also with aliasing helpers
+ * when the parameters has the property 'name'.
+ *
+ * In addition, this View class adds the plugin 'backstage' to the list of paths
+ * where the CakePHP core will look for layout files.
+ *
+ * And, finally, it deals with the $type variable on recursive calls of elements.
  */
-	function &_loadHelpers(&$loaded, $helpers, $parent = null) 
-	{
+class TypeView extends View {
+
+	/**
+	 * Overwrites the default loading process to deal with asterisks
+	 *
+	 * @param array  $loaded
+	 * @param array  $helpers
+	 * @param string $parent
+	 *
+	 * @return array
+	 */
+	function &_loadHelpers (&$loaded, $helpers, $parent = null) {
 		$layout_scheme = $this->getVar('layout_scheme');
-		if (empty($layout_scheme))
-		{
+		if (empty($layout_scheme)) {
 			trigger_error('TypeView::_loadHelpers() - `layout_scheme` was not send to view by controller.');
+
 			return parent::_loadHelpers($loaded, $helpers, $parent);
 		}
-		
+
 		$c_layout_scheme = Inflector::camelize($layout_scheme);
-		
-		$helpers_list = $helpers;
-		foreach ($helpers_list as $helper => $params)
-		{
-			if (is_string($helper) && strpos($helper,'*') !== false)   //tem que ver se pode mexer assim no �ndice do foreach, acho que sim
-			{
-				$real_helper = str_replace('*', $c_layout_scheme, $helper);
-				$helpers[$real_helper] = $params; //reformula o vetor colocando com o nome verdadeiro e os parametros
-				unset($helpers[$helper]); //desseta a chave que tem o asterisco
-				$helper = $real_helper;
+
+		$new_helpers_list = array();
+		foreach ($helpers as $helper => $params) {
+			if (is_string($helper) && strpos($helper, '*') !== false) {
+				$helperName = str_replace('*', $c_layout_scheme, $helper);
+				$new_helpers_list[$helperName] = $params;
 			}
 			else {
-				unset($helpers[$helper]);
-				$helpers[$helper] = $params;
-			}
-			
-			$res = array_search($helper, $helpers); // tira a string simples se ela existir
-			if ($res != false)
-			{
-				unset($helpers[$res]);
+				$new_helpers_list[$helper] = $params;
 			}
 		}
-	
+		$helpers = $new_helpers_list;
+
 		$loaded_helpers =& parent::_loadHelpers($loaded, $helpers, $parent);
 
-		foreach($helpers as $helper_name => $helper) //primeiro identifica aqueles que tem alias
-		{
-			if (isset($helper['name']))
-			{
-				if (strpos($helper_name, '.') !== false) 
-				{
-					list($plugin, $helper_name) = explode('.', $helper_name); //tirando o nome do plugin, para poder acessar o helper carregado
+		// identify those whom have alias and sets the name on the view object
+		foreach ($helpers as $helper_name => $helper) {
+			if (isset($helper['name'])) {
+				if (strpos($helper_name, '.') !== false) {
+					list($plugin, $helper_name) = explode('.', $helper_name);
 				}
-				if (isset($loaded_helpers[$helper_name]))
-				{
+				if (isset($loaded_helpers[$helper_name])) {
 					$loaded_helpers[$helper['name']] =& $loaded_helpers[$helper_name];
-					if (isset($loaded[$parent])) //se estamos carregando helpers para helpers, temos tamb�m que mudar o nome do helper para o helper filho
-					{
+					if (isset($loaded[$parent])) {
 						$loaded[$parent]->{$helper['name']} =& $loaded_helpers[$helper_name];
 					}
 				}
 			}
 		}
-		
+
 		return $loaded_helpers;
 	}
 
-/**
- * Overwrites the View::element() function so we can deal with the $type parameter
- * 
- * @access public
- * @return string The result of View::element()
- */
-	function element($name, $params = array(), $loadHelpers = false)
-	{
+	/**
+	 * Overwrites the View::element() function so we can deal with the $type parameter
+	 *
+	 * @access public
+	 *
+	 * @param string $name
+	 * @param array  $params
+	 * @param bool   $loadHelpers
+	 *
+	 * @return string The result of View::element()
+	 */
+	function element ($name, $params = array(), $loadHelpers = false) {
 		$typeBkp = $this->getVar('type');;
-		if (isset($params['type']))
+		if (isset($params['type'])) {
 			$this->viewVars['type'] = $params['type'];
-		
+		}
+
 		$out = parent::element($name, $params, $loadHelpers);
-		
-		$this->viewVars['type'] = $typeBkp;		
+
+		$this->viewVars['type'] = $typeBkp;
+
 		return $out;
 	}
 
-/**
- * This method overwrites the View::_paths() method so it can include the dashboard plugin path as candidate
- * 
- * @access public
- * @return array Array of possible paths from View::_paths() merged with dashboard plugin path
- */	
-	function _paths($plugin = null, $cached = true)
-	{
+	/**
+	 * This method overwrites the View::_paths() method so it can include the dashboard plugin path as candidate
+	 *
+	 * @access public
+	 *
+	 * @param string $plugin
+	 * @param bool   $cached
+	 *
+	 * @return array Array of possible paths from View::_paths() merged with dashboard plugin path
+	 */
+	function _paths ($plugin = null, $cached = true) {
 		$paths = parent::_paths($plugin, $cached);
 		array_push($paths, App::pluginPath('backstage') . 'views' . DS);
+
 		return $paths;
 	}
 }
